9baac1459215e4045b2950db624e995e04d61ee7
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-03-14 07:56:30 +0200

    create gitHub stat card component

---
+++ src/components/GitHubCard.jsx
@@ -0,0 +1,16 @@
+const GitHubCard = ({ userName }) => {
+  const srcset = `https://github-readme-stats.vercel.app/api?username=${userName}&show_icons=true`;
+  const theme = "&theme=dark";
+  return (
+    <picture>
+      <source srcset={srcset + theme} media="(prefers-color-scheme: dark)" />
+      <source
+        srcset={srcset}
+        media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)"
+      />
+      <img src={srcset} />
+    </picture>
+  );
+};
+
+export default GitHubCard;
