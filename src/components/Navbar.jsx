591e523ff6370e34d017c41b01c9de723ad5664a
DonielM <151669456+DonielM@users.noreply.github.com>
2024-03-14 19:01:35 +0000

    nav bar changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import{ HStack, Text } from "@chakra-ui/react"
+const NavBar = () => {
+  return(
+    <HStack>
+    <Text>
+      Navigation Bar
+    </Text>
+    </HStack>
+  )
+}
