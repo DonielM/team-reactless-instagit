26007e3932356302cffaf27655e93a478cedccfd
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-03-22 22:28:03 +0000

    clean unused imports + delete comments + indent

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
-import React from "react";
-import ReactDom from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
 import Header from "./components/Header.jsx";
 import Home from "./components/Home.jsx";
 import Contact from "./components/Contact.jsx";
 import "./App.css";
-// import UserCard from "./components/UserCard.jsx";
-// import Timeline from "./components/Timeline.jsx";
-// import Homepage from "./components/HomePage.jsx";
-
 
 function App() {
   return (
-      <Router>
-        <Header />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="Contact" element={<Contact />} />
-        </Routes>
-      </Router>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
