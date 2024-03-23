2736e5f2f0ba02d307348bdf72e402ce28a78563
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-03-23 15:24:09 +0000

    fix errors in naming

--- src/components/gitHubCard/GitHubCard.jsx
+++ src/components/gitHubCard/GitHubCard.jsx
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 
 const GitHubCard = ({ userName }) => {
-  const srcset = `https://github-readme-stats.vercel.app/api?username=${userName}&show_icons=true`;
+  const url = `https://github-readme-stats.vercel.app/api?username=${userName}&show_icons=true`;
   const theme = "&theme=white";
 
   return (
     <picture>
-      <source srcset={srcset + theme} media="(prefers-color-scheme: dark)" />
+      <source srcSet={url + theme} media="(prefers-color-scheme: dark)" />
       <source
-        srcset={srcset}
+        srcSet={url}
         media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)"
       />
-      <img src={srcset} />
+      <img src={url} />
     </picture>
   );
 };
