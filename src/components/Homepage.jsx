59cc1440cbeac8ab1209d570f6acc4b0cfc325ae
vicky wong <vicky.boci@gmail.com>
2024-03-25 16:28:35 +0800

    test the token message

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../styles/Homepage.css";
 import Sidenav from "./Sidenav";
 import Timeline from "./Timeline";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Homepage() {
   return (
@@ -9,8 +11,14 @@ function Homepage() {
       {/* <div className="homepage__navWraper">
         <Sidenav />
       </div> */}
-      <div className="homepage__timeline container">
-        <Timeline />
+      
+      <div className="homepage__timeline container-fluid">
+        <div className="row">
+          <div className="col-lg-12">
+            <Timeline />
+          </div>
+        </div>
+
       </div>
     </div>
   );
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import Post from "./Posts";
 import "../styles/Timeline.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Timeline() {
-  const [posts, setPosts] = useState([
+  const posts=[
     {
       user: "redian_",
+      key:1,
       postImage:
         "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
       likes: 54,
-      timestamp: "2d",
+      timestamp1: "2d",
     },
     {
       user: "johndoe",
+      key:2,
       postImage:
         "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80",
       likes: 432,
@@ -20,6 +24,7 @@ function Timeline() {
     },
     {
       user: "mariussss",
+      key:3,
       postImage:
         "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
       likes: 140,
@@ -27,30 +32,44 @@ function Timeline() {
     },
     {
       user: "kobee_18",
+      key:4,
       postImage:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGCAaQ5u1TMTij5ELPWi5-VPtlSqELw-R6lj0EpYmNcGt56kOQaCokzS0IK81MOSphlkw&usqp=CAU",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
       likes: 14,
       timestamp: "2d",
     },
-  ]);
+  ];
 
+  const chunkArray = (arr, size) => {
+    const chunkedArray = [];
+    for (let i = 0; i < arr.length; i += size) {
+      chunkedArray.push(arr.slice(i, i + size));
+    }
+    return chunkedArray;
+  };
+
+  const postsRows = chunkArray(posts, 4);
   return (
-    <div className="timeline">
-      <div className="timeline__left">
-        <div className="timeline__posts">
-          {posts.map((post) => (
+    <div className="timeline container-fluid">
+      {postsRows.map((row, index) => (
+        <div key={index} className="row mb-3">
+          {row.map((post, postIndex) => (
+            <div key={postIndex} className="col-lg-6 d-flex justify-content-center">
+          
             <Post
               user={post.user}
               postImage={post.postImage}
               likes={post.likes}
               timestamp={post.timestamp}
             />
-          ))}
+            </div>
+          ))}  
         </div>
-      </div>
-      <div className="timeline__right">
-      </div>
+          ))}  
     </div>
+      
+
+    
   );
 }
 
